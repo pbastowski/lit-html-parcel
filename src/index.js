d75a933aba998fff0bb9565f8d07eddd66bd68b9
Paul Bastowski <pbastowski@gmail.com>
2019-09-15 08:56:27 +0200

    Add a non-lit-element App rendering wrapper for testing purposes Discovered that generated (with map) elements don't initialise the value prop correctly on radios. Instead of the expected value we get "on", until after the first rendering. I suspect this has to do with the way lit-html created elements.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import { LitElement, html } from 'lit-element'
+import { render } from 'lit-html'
 import { observe, computed, dispose } from 'hyperactiv/dist'
 
 import store from './store'
 import App from './App'
+
+// render(App(), document.querySelector('lit-02'))
+// computed(() => render(App(), document.querySelector('lit-02')))
 
 customElements.define(
     'lit-02',
     class extends LitElement {
         $$store = computed(() => {
             this.template = App()
-            this.requestUpdate()
+            queueMicrotask(() => {
+                this.requestUpdate()
+            })
         })
+
         render() {
+            console.log('RENDER')
             return this.template
+        }
+
+        disconnectedCallback() {
+            dispose(this.$$store)
         }
     }
 )
