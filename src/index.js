f548938f35970dfda31ceb44883a897e787a0590
Paul Bastowski <pbastowski@gmail.com>
2019-09-18 11:29:49 +0200

    Remove the unnecessary double render, now that radios render properly the first time. Remove unused code. Alias store to hyperactiv/dist, because hyperactive does not export the methods we need and we can't alias hyperactiv to hyperactiv.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-import { LitElement, html } from 'lit-element'
+import { LitElement } from 'lit-element'
 import { render } from 'lit-html'
-import { observe, computed, dispose } from 'hyperactiv/dist'
+import { computed, dispose } from 'store'
 
-import store from './store'
 import App from './App'
 
-// render(App(), document.querySelector('lit-02'))
-// computed(() => render(App(), document.querySelector('lit-02')))
+computed(() => render(App(), document.querySelector('lit-02')))
 
-customElements.define(
-    'lit-02',
-    class extends LitElement {
-        $$store = computed(() => {
-            this.template = App()
-            queueMicrotask(() => {
-                this.requestUpdate()
-            })
-        })
-
-        render() {
-            console.log('RENDER')
-            return this.template
-        }
-
-        disconnectedCallback() {
-            dispose(this.$$store)
-        }
-    }
-)
+// customElements.define(
+//     'lit-02',
+//     class extends LitElement {
+//         $$store = computed(() => {
+//             this.template = App()
+//             this.requestUpdate()
+//         })
+//
+//         render() {
+//             return this.template
+//         }
+//
+//         disconnectedCallback() {
+//             dispose(this.$$store)
+//         }
+//     }
+// )
