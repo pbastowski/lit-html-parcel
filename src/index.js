3595a2ca8e7263065a7844590f0fedcc2dbe7f61
Paul Bastowski <pbastowski@gmail.com>
2019-09-19 16:12:11 +0200

    Pass props set on the lit-element wrapped custom element to the rest of the app Make passed in props reactive in the apps store.

--- src/index.js
+++ src/index.js
@@ -2,24 +2,40 @@ import { LitElement } from 'lit-element'
 import { render } from 'lit-html'
 import { computed, dispose } from 'store'
 
+import store from './store'
+
 import App from './App'
 
-computed(() => render(App(), document.querySelector('lit-02')))
+// computed(() => render(App(), document.querySelector('lit-02')))
 
-// customElements.define(
-//     'lit-02',
-//     class extends LitElement {
-//         $$store = computed(() => {
-//             this.template = App()
-//             this.requestUpdate()
-//         })
-//
-//         render() {
-//             return this.template
-//         }
-//
-//         disconnectedCallback() {
-//             dispose(this.$$store)
-//         }
-//     }
-// )
+customElements.define(
+    'lit-02',
+    class extends LitElement {
+        static get properties() {
+            return {
+                abc: String
+            }
+        }
+
+        updated(changes) {
+            // Sync the store with the prop (if that'w what you need)
+            changes.forEach((ov, prop) => {
+                store.state[prop] = this[prop]
+            })
+        }
+
+        $$store = computed(() => {
+            // Passing this to App gives App to refer to any properties
+            this.template = App(this)
+            this.requestUpdate()
+        })
+
+        render() {
+            return this.template
+        }
+
+        disconnectedCallback() {
+            dispose(this.$$store)
+        }
+    }
+)
